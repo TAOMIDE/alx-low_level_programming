ae95afe2e69d7ffa9ad6b719c33f2ca3723a396e
Akinbami <tfkakinbami80@gmail.com>
2022-09-22 17:19:41 -0700

    Write a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,23 +18,20 @@ char *cap_string(char *s)
 	{/* if next character after count is a char , capitalise it */
 		if (s[0] >= 97 && s[0] <= 122)
 		{
-			if (s[0] >= 97 && s[0] <= 122)
-			{
-				s[0] = s[0] - 32;
-			}
-			if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-					|| s[count] == ',' || s[count] == ';' || s[count] == '.'
-					|| s[count] == '.' || s[count] == '!' || s[count] == '?'
-					|| s[count] == '"' || s[count] == '(' || s[count] == ')'
-					|| s[count] == '{' || s[count] == '}')
-			{
-				if (s[count + 1] >= 97 && s[count + 1] <= 122)
-				{
-					s[count + 1] = s[count + 1] - 32;
-				}
-			}
-			count++;
+			s[0] = s[0] - 32;
 		}
-		return (s);
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+			|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+			|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+			|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+			|| s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
 	}
+	return (s);
 }
